133503e9912beb5753ad6e4416655fc61acbfa19
Shivam Kumar Gupta <shivam@mys-MacBook-Pro.local>
2018-09-08 15:30:50 -0700

    Basic Array Operations

---
+++ list/array.md
@@ -0,0 +1,40 @@
+# Array Operations
+
+```array.py``` file shows various array operations performed on list in python.
+
+Python by default do not hava array data structure. The data structure present in python is ```list```.
+
+## How Array in python is different from other languages such as  ```Java```.
+
+        1. The List is not defined with number of items inside it. In other languages the ArrayList is defined with the maximum number of itmes in the ArrayList.
+        2. Since List has no memory boundations while initializing, it can store more than 1 types of Data Structure inside it.
+        3.There are various operations we can perform on the list to solve various problems withput writting complex logic.
+
+## Time Complexity of insert, delete, search and update operations in Python.
+
+```insert```
+
+```
+To insert a element at the end of the List the time Complexity is o(1)
+To insert a element at starting or middle of the List the time Complexity is o(n).
+```
+
+```delete```
+
+```
+To delete a element at the end of the List the time Complexity is o(1)
+To delete a element at starting or middle of the List the time Complexity is o(n).
+```
+
+```search```
+
+```
+Searching a list in python is completed on o(n).
+Searching with index is completed in constant time o(1).  
+```
+
+```update```
+
+```
+Updatingan item is completed in constant time O(1)
+```
---
+++ list/array.py
@@ -0,0 +1,117 @@
+# Testing out different Operations in Array. 
+
+# Initializing 1-d array.
+nums = []
+print(nums)
+print('*********************************')
+
+# Initializing List with Values.
+nums = [1,2,3,4,5]
+print(nums)
+print('*********************************')
+
+# Initializing List with multiple data structure. 
+nums = [1,2,3,'four','five','six']
+print(nums)
+print('*********************************')
+
+# Initializing a 2-d List
+nums = [[1,2,3,4,5,6],
+        [1,2,3,4,5,6],
+        [1,2,3,4,5,6]
+        ]
+print(nums)
+print('*********************************')
+
+# Iterating through List.
+# 1. Enhanced For loop
+
+nums = [1,2,3,4,5]
+for item in nums:
+        print(item)
+print('*********************************')
+
+# Finding out length of List.
+print(len(nums))
+print('*********************************')
+
+# Iterating through list with range
+for i in range(len(nums)):
+        print(nums[i])
+print('*********************************')
+
+# Append a List, equivalent to nums[len(nums):] = [item]
+nums.append(6)
+# or
+nums[len(nums):] = [7]
+print(nums)
+print('*********************************')
+
+# Add another iterable to a List. Can use + operator instead of extend
+print(nums+['one','two','three','four','five','six','seven'])
+print(nums)
+print('*********************************')
+# or
+nums.extend(['one','two','three','four','five','six','seven'])
+print(nums)
+print('*********************************')
+
+# Inserting at specific location.
+# nums.insert(arg1,arg2)
+# arg1 = index at which the value is to inserted.
+# arg2 = value to be inserted. 
+nums.insert(14,8)
+print(nums)
+print('*********************************')
+
+# Removing element from List.
+# .remove(x) will remove the first element from the list. 
+# If the element is not found "ValueError" is raised. 
+
+nums.remove(8)
+try:
+        nums.remove(14)
+except ValueError as exception:
+        print("Exception occured while removing item: " + str(exception))
+
+print('*********************************')
+
+# pop([i]) also removes an element from the list and returns it. 
+# If no index is specified then the last element is removed.
+
+print(nums.pop())
+print(nums.pop(0))
+try:
+        print(nums.pop(111))
+except IndexError as exception:
+        print("Exception Occured: " + str(exception))
+print('*********************************')
+
+# .clear will clear the while list.
+# Uncomment it.
+# nums.clear()
+print(nums)
+print('*********************************')
+
+# Finding the index of a given element 
+# .index(x)
+# ValueError is raised if no such element is found. 
+print(nums.index(2))
+print('*********************************')
+
+# Counting the occurance of a given element.
+print(nums.count(2))
+print('*********************************')
+
+# Reversing an List
+nums.reverse()
+print(nums)
+print('*********************************')
+
+# Sorting an List
+nums = [3,2,4,6,7,8,1]
+nums.sort()
+print(nums)
+nums.sort(reverse = True)
+print(nums)
+print('*********************************')
